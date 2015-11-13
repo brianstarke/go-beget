451211c5f55ba5ab8f05caa63c184eda13aa4dd5
brianstarke <brian.starke@gmail.com>
2015-11-13 10:55:47 -0500

    clarified struct data struct

--- generator/inspector.go
+++ generator/inspector.go
@@ -2,10 +2,33 @@ package generator
 
 import "go/ast"
 
-// GeneratorInfo is a thing
-type GeneratorInfo struct {
+/*
+StructData contains data about this struct that is used during the code
+generation, such as the struct name and package name such that we could
+generate things like
+
+	package foo
+
+	type BarSearchRequest struct {
+		// ...
+	}
+
+StructData also collects the Fields contained with the struct such that we
+can generate a list of fields that are approved for search or create or update
+requests like
+
+ 	type BarField int
+
+	const (
+		Baz BarField = iota
+		Boom
+		Biff
+		Bang
+	)
+*/
+type StructData struct {
+	Name          string    // the name of the struct we're looking for
 	PkgImportPath string    // the import path for this type
-	TypeName      string    // name of the type we're looking for
 	File          *ast.File // parsed file containing our struct def
 	Fields        []Field   // the fields def
 	inType        bool      // private helper for when we are walking the AST
@@ -13,6 +36,6 @@ type GeneratorInfo struct {
 
 // Field is a thing
 type Field struct {
-	Name string
-	Tags map[string]string
+	Name string            // name of the field
+	Tags map[string]string // tags on the field
 }
