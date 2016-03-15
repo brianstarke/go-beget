fcb6494b6d53aea2abfb5575172e4a472ede2bb5
brianstarke <brian.starke@gmail.com>
2016-03-15 15:21:18 -0400

    added travis build

--- generator/parser_test.go
+++ generator/parser_test.go
@@ -31,7 +31,7 @@ func TestGetImportPath(t *testing.T) {
 	path, err := getImportPath()
 
 	assert.NoError(err, "should not error when trying to find the import path")
-	assert.Equal("gitlab.com/brianstarke/go-begetter/generator", path)
+	assert.Equal("github.com/brianstarke/go-beget/generator", path)
 }
 
 func TestParseStructFields(t *testing.T) {
